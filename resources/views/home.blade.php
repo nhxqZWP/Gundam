ff03c50b640a3cddf523308e8ec99d8303ca376a
weipeng <xiaowei719899730@163.comm>
2019-09-22 00:23:37 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">控制台</div>
+                <div class="panel-heading">BTC-USD-SWAP</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    BTC-USD-SWAP: {{ $price }}
+                    Price: {{ $price }}
                 </div>
             </div>
         </div>
